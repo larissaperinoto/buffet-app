1bfd6204b8062a80b2903ac37e727ee31e04ad17
Larissa Perinoto <perinotolarissa@gmail.com>
2024-07-08 16:31:43 -0300

    refactor: altera nome da porta da balança

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,14 +4,17 @@ import { Scale, Units } from "./services/Scale";
 
 const scale_1 = new Scale(
   {
-    path: "/dev/ttyBalanca",
+    path: "/dev/ttyScale",
     baudRate: 57600,
     autoOpen: true,
   },
   Units.G
 );
 
-const buffet_1 = new Buffet(50, 29.9, [scale_1]);
+const perKgPrice = 59.9;
+const openPrice = 29.9;
+
+const buffet_1 = new Buffet(perKgPrice, openPrice, [scale_1]);
 
 const order = new Meal(buffet_1);
 
